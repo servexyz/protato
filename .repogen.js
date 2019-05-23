2893d6cd2e4cd33ed29c2e2ab9d4917d67f80b48
alechp <me@alechp.com>
2019-05-23 12:57:53 -0700

    Added repogen config & symlinks

---
+++ .repogen.js
@@ -0,0 +1,10 @@
+const config = {
+  provider: "alechp",
+  repospacePath: __dirname,
+  repositories: [
+    { servexyz: "protato-lib" },
+    { servexyz: "protato-cli"},
+  ]
+};
+
+module.exports = { config };
